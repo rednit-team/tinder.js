8ad0e9e89411207d42e8df00e02e35de97434861
Mathis Burger <mburger@die-netzwerkstatt.de>
2022-01-17 19:16:47 +0100

    [TASK] fixed eslint

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/// ///////////////////////////////////////////////////
+//                  Tinder.js                       //
+// This is the entrypoint of the tinder.js api      //
+// wrapper. If you have any questions feel free to  //
+// open an issue.                                   //
+/// ///////////////////////////////////////////////////
